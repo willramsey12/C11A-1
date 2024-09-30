5a6af0d13488abbc4bb07cc08e50248339bcc7db
Aidan Paul <aidanpaul102103@gmail.com>
2024-09-29 23:39:54 -0600

    Changed mouth color from pink to black

--- src/DrawPlay/Cat.java
+++ src/DrawPlay/Cat.java
@@ -38,7 +38,7 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.black); //changed mouth color to black
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
